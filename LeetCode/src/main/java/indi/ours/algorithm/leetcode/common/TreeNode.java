a3fd26135ed0d3754d4301805e6d16dd2c1ecce5
hbnking <hbn.king@gmail.com>
2018-10-09 12:42:03 +0800

    add TreeNode.java   to common package

---
+++ LeetCode/src/main/java/indi/ours/algorithm/leetcode/common/TreeNode.java
@@ -0,0 +1,13 @@
+package indi.ours.algorithm.leetcode.common;
+
+/**
+ * @author wangheng
+ * @create 2018-10-09 下午12:17
+ * @desc
+ **/
+public class TreeNode {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int x) { val = x; }
+}
